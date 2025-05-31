b4dcedb46e5f45cf5138e669553f7220352316ea
jose781 <146405410+jfernando234@users.noreply.github.com>
2025-05-30 20:20:18 -0500

    Esquema base

---
+++ src/app/core/card-product/card-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-product',
+  imports: [],
+  templateUrl: './card-product.component.html',
+  styleUrl: './card-product.component.css'
+})
+export class CardProductComponent {
+
+}
---
+++ src/app/core/core.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-core',
+  imports: [],
+  templateUrl: './core.component.html',
+  styleUrl: './core.component.css'
+})
+export class CoreComponent {
+
+}
