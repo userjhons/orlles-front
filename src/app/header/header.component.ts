7f04d6a88a738d545e03f8d19a37dc3fc0ca90f7
S0reee <dongosebastian@gmail.com>
2025-05-31 01:07:57 -0500

    Header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  standalone: true, 
+  imports: [CommonModule, FormsModule] 
 })
 export class HeaderComponent {
+  @Input() cartItemCount: number = 0;
+  @Input() isLoggedIn: boolean = false;
+  @Input() userName: string = '';
+  
+  @Output() loginClick = new EventEmitter<void>();
+  @Output() cartClick = new EventEmitter<void>();
+  @Output() logout = new EventEmitter<void>();
+  @Output() search = new EventEmitter<string>();
+  
+  searchTerm: string = '';
 
-
+  onLoginClick() {
+    this.loginClick.emit();
+  }
+  
+  onCartClick() {
+    this.cartClick.emit();
+  }
+  
+  onLogout() {
+    this.logout.emit();
+  }
+  
+  onSearch() { 
+    if (this.searchTerm.trim()) {
+      this.search.emit(this.searchTerm.trim());
+    }
+  }
 }
